bd9b1090d6af5cc1a97cf3792eb5c62b78fd5895
jareddishman <76022962+jareddishman@users.noreply.github.com>
2023-12-03 19:29:11 -0800

    Update console arguments
    Added --save to choose when model weights are saved to file
    
    Added --p to choose if predictions are printed to console

--- run.py
+++ run.py
@@ -5,27 +5,29 @@ import src.models.train_model as train_model
 import src.models.predict_model as predict
 
 def main():
-    parser = argparse.ArgumentParser(description='Train, test, and predict using a model.')
+    parser = argparse.ArgumentParser(description='Predict whether a peptide has antimicrobial properties using a Deep Neural Network')
     parser.add_argument('--mode', choices=['train', 'test', 'predict'], nargs='+', default=['predict'], help='Specify the mode(s) to run (train, test, predict)')
 
-    # Sub-argument for train mode
+    # Sub-arguments for train mode
     train_parser = parser.add_argument_group('train mode options')
     train_parser.add_argument('--trainset', choices=['train', 'test', 'evaluate'], nargs='+', default=['train'], help='Specify the subset(s) of the data to use in training')
+    train_parser.add_argument('--save', action='store_true', help='[Optional] Choose whether to save the model weights')
 
-    # Sub-argument for test mode
+    # Sub-arguments for test mode
     test_parser = parser.add_argument_group('test mode options')
-    test_parser.add_argument('--testset', choices=['train', 'test', 'evaluate'], nargs='+', default=['train'], help='Specify the subset(s) of the data to use in testing')
+    test_parser.add_argument('--testset', choices=['train', 'test', 'evaluate'], nargs='+', default=['test'], help='Specify the subset(s) of the data to use in testing')
 
-    # Add other arguments as needed
-    # parser.add_argument('--save', action='store_true', help='Toggle checkpoint saving')
-    parser.add_argument('--f', help='Specify the file path for .fa file when mode is predict')
+    # Sub-arguments for predict mode
+    test_parser = parser.add_argument_group('predict mode options')
+    test_parser.add_argument('--f', help='Specify the file path for .fa file')
+    test_parser.add_argument('--p', action='store_true', help='[Optional] Choose whether to print predictions to console')
+    # test_parser.add_argument('--s', default='/', help='[Optional] Saves predictions to given filepath')
 
     args = parser.parse_args()
 
     if 'train' in args.mode:
-        # TODO: Add --save argument to toggle checkpoint saving
         training_dataset = build.create_dataset(args.trainset)  # train, eval, test
-        model = train_model.train_model(training_dataset)
+        model = train_model.train_model(training_dataset, save_checkpoint=args.save)
     else:
         model = train_model.load_model()
     
@@ -39,7 +41,13 @@ def main():
             parser.error('Prediction requires a filepath to be specified with --f')
         else:
             # Given a file, output predictions
-            print(predict.predict_from_file(model, args.f))
+            prediction = predict.predict_from_file(model, args.f)
+            if args.p:
+                print(prediction)
+            # if args.s:
+            #     # TODO: Save prediction to filepath
+            #     ...
+
 
 if __name__ == '__main__':
     main()
