cc7150de73a80784a598e17d413492bc7b36067c
e2pak <91354875+e2pak@users.noreply.github.com>
2023-12-07 03:42:47 -0800

    Update predict_model.py

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,10 +1,16 @@
 from src.features.build_datasets import vectorize
 
 def evaluate_model(model, dataset):
+    # Evaluate model on provided dataset and get test accuracy
     test_accuracy = model.evaluate(dataset, batch_size=8)
     return test_accuracy
 
 def predict_from_file(model, filepath):
+    # vectorize sequence data from the dataset file
     vectors = vectorize(filepath)
+
+    # Make predictions from model
     predictions = model.predict(vectors)
+
+    # Convert probability predictions to binary labels (0 or 1) based on a threshold of 0.5
     return [1 if p > 0.5 else 0 for p in predictions]
