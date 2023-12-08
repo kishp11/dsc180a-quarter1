c81d60665242b23a7e28d2d19af8ada3c0b530ad
jareddishman <76022962+jareddishman@users.noreply.github.com>
2023-12-07 21:07:36 -0800

    metrics experimenting

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,7 +1,42 @@
 import keras
 from keras import layers
+import keras.backend as K
 
 checkpoint_filepath = 'src/models/checkpoints/model_weights'
+
+def __tpnp(y_true, y_pred):
+    y_true = K.round(y_true)  # Convert to binary values (0 or 1)
+    y_pred = K.round(y_pred)
+
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    true_negatives = K.sum(K.round(K.clip((1 - y_true) * (1 - y_pred), 0, 1)))
+    
+    false_positives = K.sum(K.round(K.clip((1 - y_true) * y_pred, 0, 1)))
+    false_negatives = K.sum(K.round(K.clip(y_true * (1 - y_pred), 0, 1)))
+    
+    return true_positives,true_negatives,false_positives,false_negatives
+
+def __sens(y_true, y_pred):
+    # Assuming binary classification
+    true_positives, true_negatives, false_positives, false_negatives = __tpnp(y_true, y_pred)
+    sens = true_positives/(true_positives + false_negatives + K.epsilon()) * 100 # K.epsilon() is a small constant that prevents divide by zero errors
+    return sens
+
+def __spec(y_true, y_pred):
+    true_positives, true_negatives, false_positives, false_negatives = __tpnp(y_true, y_pred)
+    spec = true_negatives/(true_negatives + false_positives + K.epsilon()) * 100
+    return spec
+
+def __acc(y_true, y_pred):
+    true_positives, true_negatives, false_positives, false_negatives = __tpnp(y_true, y_pred)
+    acc = (true_positives + true_negatives)/(true_positives + false_positives + true_negatives + false_negatives + K.epsilon()) * 100
+    return acc
+
+def __mcc(y_true, y_pred):
+    true_positives, true_negatives, false_positives, false_negatives = __tpnp(y_true, y_pred)
+    mcc = (((true_positives * true_negatives) - (false_negatives * false_positives)) / 
+            K.sqrt((true_positives + false_negatives) * (true_negatives + false_positives) * (true_positives + false_positives) * (true_negatives + false_negatives)) + K.epsilon())
+    return mcc
 
 def __create_model():
     # define sequential model
@@ -17,11 +52,11 @@ def __create_model():
 
     # Compile model using binary crossentropy loss and Adam optimizer
     adam = keras.optimizers.Adam()
-    model.compile(loss='binary_crossentropy', metrics = 'accuracy', optimizer=adam)
-
+    model.compile(loss='binary_crossentropy', metrics = [__sens, __spec, __acc, __mcc], optimizer=adam)
+    
     return model
 
-def train_model(data_train, batch_size=8, epochs=10, save_checkpoint=True):
+def train_model(data_train, batch_size=8, epochs=10, save_checkpoint=False):
     '''
     trains the model on provided training dataset
     '''
@@ -30,11 +65,15 @@ def train_model(data_train, batch_size=8, epochs=10, save_checkpoint=True):
     model.fit(data_train, batch_size=batch_size, epochs=epochs)
 
     if save_checkpoint:
+        # Saves model weights to models/checkpoints/
         model.save_weights(checkpoint_filepath)
 
     return model
 
 def load_model():
+    """
+    Loads model weights from checkpoint file
+    """
     model = __create_model()
-    model.load_weights(checkpoint_filepath).expect_partial()
+    model.load_weights(checkpoint_filepath).expect_partial() # expect_partial as otherwise many warnings thrown in console
     return model
