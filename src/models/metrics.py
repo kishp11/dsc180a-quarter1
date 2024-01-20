bf4139678120879ee694862fb51352b5fda7aa54
jareddishman <76022962+jareddishman@users.noreply.github.com>
2024-01-20 14:14:17 -0800

    Update metrics.py
    fixed bug

--- src/models/metrics.py
+++ src/models/metrics.py
@@ -7,30 +7,30 @@ tn = TrueNegatives()
 fn = FalseNegatives()
 
 # true and false positives and negatives
-def __TPTNFPFN(y_true, y_pred):
+def TPTNFPFN(y_true, y_pred):
     return tp(y_true, y_pred), tn(y_true, y_pred), fp(y_true, y_pred), fn(y_true, y_pred)
 
 # sensitivity/recall metric
-def __sens(y_true, y_pred):
+def sens(y_true, y_pred):
     # Assuming binary classification
     true_positives, true_negatives, false_positives, false_negatives = __TPTNFPFN(y_true, y_pred)
     sens = true_positives/(true_positives + false_negatives + K.epsilon()) * 100 # K.epsilon() is a small constant that prevents divide by zero errors
     return sens
 
 # specificity metric
-def __spec(y_true, y_pred):
+def spec(y_true, y_pred):
     true_positives, true_negatives, false_positives, false_negatives = __TPTNFPFN(y_true, y_pred)
     spec = true_negatives/(true_negatives + false_positives + K.epsilon()) * 100
     return spec
 
 # accuracy metric
-def __acc(y_true, y_pred):
+def acc(y_true, y_pred):
     true_positives, true_negatives, false_positives, false_negatives = __TPTNFPFN(y_true, y_pred)
     acc = (true_positives + true_negatives)/(true_positives + false_positives + true_negatives + false_negatives + K.epsilon()) * 100
     return acc
 
 # matthews correlation coefficient
-def __mcc(y_true, y_pred):
+def mcc(y_true, y_pred):
     true_positives, true_negatives, false_positives, false_negatives = __TPTNFPFN(y_true, y_pred)
     mcc = (((true_positives * true_negatives) - (false_negatives * false_positives)) / 
             K.sqrt((true_positives + false_negatives) * (true_negatives + false_positives) * (true_positives + false_positives) * (true_negatives + false_negatives)) + K.epsilon())
