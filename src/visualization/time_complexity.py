33412ebc9a5d4f699d3c3cc9d9e9f40d4325a8cc
jareddishman <76022962+jareddishman@users.noreply.github.com>
2024-01-20 14:19:53 -0800

    written outline

---
+++ src/visualization/time_complexity.py
@@ -0,0 +1,8 @@
+# Create fake datasets of various sizes
+
+# Run each fake dataset through model
+    # output time taken in milliseconds
+
+# plot time taken
+    # x = dataset size
+    # y = time taken
