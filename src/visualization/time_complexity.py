facf979099070a14cf141a70bc0322bfe8eab432
Jiyong <jik032@ucsd.edu>
2024-01-20 14:57:53 -0800

    fake dataset

--- src/visualization/time_complexity.py
+++ src/visualization/time_complexity.py
@@ -1,5 +1,21 @@
 # Create fake datasets of various sizes
 
+# global variable to determine fake dataset length
+import random
+def create_fake_datasets(vector_length, dataset_size):
+    vector_length = vector_length
+    dataset_size = dataset_size
+    data = np.array([])
+    for i in dataset_size:
+        sequence = np.array([])
+        padding = 200 - vector_length
+        for x in padding:
+            sequence.append(0)
+        for j in vector_length:
+            sequence.append(random.randint(1,20))
+        data.append(sequence)
+    return data
+
 # Run each fake dataset through model
     # output time taken in milliseconds
 
