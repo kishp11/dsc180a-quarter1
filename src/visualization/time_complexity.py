dd1905b554e8c69fce772685cb5219e0b82b1be2
Jiyong <jik032@ucsd.edu>
2024-01-20 16:15:05 -0800

    stuffs

--- src/visualization/time_complexity.py
+++ src/visualization/time_complexity.py
@@ -10,7 +10,7 @@ def create_fake_datasets(sizes, vector_length=150):
     all_datasets = np.array([])
     for size in sizes:
         data = create_single_dataset(size, vector_length)
-        all_datasets = np.append(all_datasets, data)
+        all_datasets = np.append(all_datasets, data, axis = 0)
     return all_datasets
 
 def create_single_dataset(dataset_size, vector_length):
@@ -22,7 +22,7 @@ def create_single_dataset(dataset_size, vector_length):
         for j in range(vector_length):
             rng = np.append(rng, random.randint(1,20))
         sequence = np.append(rng, sequence)
-        data = np.append(data, sequence)
+        data = np.append(data, sequence, axis = 0)
     return data
 
 # Run each fake dataset through model
